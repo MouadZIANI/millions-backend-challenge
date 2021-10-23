567f81af8bda0fe1bfb6d62399c470b944c0ba83
MouadZIANI <mouad.ziani1997@gmail.com>
2021-10-23 14:19:13 +0100

    tests: assert for like/unlike

--- tests/Feature/PostsActionsTest.php
+++ tests/Feature/PostsActionsTest.php
@@ -5,12 +5,25 @@ namespace Tests\Feature;
 use App\Models\Post;
 use App\Models\PostLike;
 use App\Models\User;
+use App\Repositories\Posts\EloquentPostRepository;
+use App\Repositories\Posts\PostRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class PostsActionsTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @var \App\Repositories\Posts\PostRepository */
+    private PostRepository $postRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->postRepository = new EloquentPostRepository(new Post);
+    }
 
     /** @test */
     public function it_list_posts_paginated_with_likes_count_and_latest_five_reacters()
@@ -63,5 +76,25 @@ class PostsActionsTest extends TestCase
             ->assertJsonPath('posts.1.uuid', $post2->uuid)
             ->assertJsonPath('posts.1.likes_count', 0)
             ->assertJsonPath('posts.1.reacters', []);
+    }
+
+    /** @test */
+    public function an_authenticated_can_like_or_unlike_post()
+    {
+        $user = User::factory()->create();
+
+        $post = Post::factory()
+            ->for($user, 'author')
+            ->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('api:v1:posts:toggle-like', $post->uuid));
+
+        $response->assertNoContent();
+
+        $likes = $this->postRepository->findPostLikesByUuid($post->uuid);
+
+        $this->assertEquals(1, $likes->count());
+        $this->assertEquals($likes->first()->user_id, $user->uuid);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function actingAs($user, $driver = null)
+    {
+        $token = JWTAuth::fromUser($user);
+        $this->withHeader('Authorization', 'Bearer ' . $token);
+
+        return $this;
+    }
 }
