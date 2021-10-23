649ceb3af74ffdd3287fb6d12e4289081bbc89de
MouadZIANI <mouad.ziani1997@gmail.com>
2021-10-23 15:39:59 +0100

    tests: assert if user can create new post and post created notification sent to other users

--- tests/Feature/PostsActionsTest.php
+++ tests/Feature/PostsActionsTest.php
@@ -5,10 +5,14 @@ namespace Tests\Feature;
 use App\Models\Post;
 use App\Models\PostLike;
 use App\Models\User;
+use App\Notifications\PostPublished;
 use App\Repositories\Posts\EloquentPostRepository;
 use App\Repositories\Posts\PostRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class PostsActionsTest extends TestCase
@@ -96,5 +100,91 @@ class PostsActionsTest extends TestCase
 
         $this->assertEquals(1, $likes->count());
         $this->assertEquals($likes->first()->user_id, $user->uuid);
+
+        $this->get(route('api:v1:posts:toggle-like', $post->uuid));
+
+        $likes = $this->postRepository->findPostLikesByUuid($post->uuid);
+
+        $this->assertEquals(0, $likes->count());
+        $this->assertEmpty($likes);
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_delete_only_his_posts_with_stored_images()
+    {
+        Storage::fake('local');
+
+        $user = User::factory()->create();
+
+        $post = Post::factory()
+            ->for($user, 'author')
+            ->create([
+                'image' => UploadedFile::fake()->image('post.jpg')->size(100)->store('posts')
+            ]);
+
+        $imagePath = $post->image;
+
+        $this->assertTrue(Storage::exists($imagePath));
+
+        $response = $this->actingAs($user)
+            ->delete(route('api:v1:posts:delete', $post->uuid));
+
+        $response->assertNoContent();
+
+        $this->assertNull($this->postRepository->findByUuid($post->uuid));
+        $this->assertFalse(Storage::exists($imagePath));
+    }
+
+    /** @test */
+    public function it_throws_an_unauthorized_error_if_the_authenticated_user_try_to_delete_unrelated_post()
+    {
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        $post = Post::factory()
+            ->for($user1, 'author')
+            ->create([
+                'image' => UploadedFile::fake()->image('post.jpg')->size(100)->store('posts')
+            ]);
+
+        $response = $this->actingAs($user2)
+            ->delete(route('api:v1:posts:delete', $post->uuid));
+
+        $response->assertForbidden();
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_create_a_new_post_and_post_created_notification_sent_to_other_users()
+    {
+        Storage::fake('local');
+        Queue::fake();
+        Notification::fake();
+
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        $response = $this->actingAs($user1)
+            ->post(route('api:v1:posts:store'), [
+                'description' => 'Post of test',
+                'image' => UploadedFile::fake()->image('post.jpg')->size(100)
+            ]);
+
+        Notification::assertSentTo(
+            [$user2], PostPublished::class
+        );
+
+        Notification::assertNotSentTo(
+            [$user1], PostPublished::class
+        );
+
+        $response->assertCreated()
+            ->assertJsonStructure(['post'])
+            ->assertJsonStructure(['post' => ['uuid', 'description', 'image', 'date', 'likes_count']])
+            ->assertJsonPath('post.description', 'Post of test');
+    }
+
+    public function ()
+    {
+        
     }
 }
